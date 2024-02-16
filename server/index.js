3c9597647381a9962bf013633eb1236348257e82
vanshjangir <vanshjangir0001@gmail.com>
2024-02-16 15:50:46 +0530

    job show,desc done

--- server/index.js
+++ server/index.js
@@ -5,21 +5,8 @@ const port = 3000
 const {MongoClient} = require('mongodb')
 const uri = 'mongodb+srv://hackOharbour:D1kVfg4XSaaq3sUX@cluster0.g1ic6ja.mongodb.net/?retryWrites=true&w=majority';
 
-const fs = require('fs');
 const client = new MongoClient(uri);
-
-
-
-
-
-
-
-
-
-
-
-
-
+const db = client.db('hackOharbour');
 
 app.use(express.json());
 
@@ -31,13 +18,12 @@ app.post('/', async (req, res) => {
 app.post('/login', async (req, res) => {
 
   const userdata = req.body;
-  const db = client.db('hackOharbour');
   const users = db.collection('users');
   const userarr = await users.find({}).toArray();
 
-  const founduser = userarr.find(x => x.username === userdata.username);
-  if(founduser){
-    if(founduser.password === userdata.password){
+  const foundUser = userarr.find(x => x.username === userdata.username);
+  if(foundUser){
+    if(foundUser.password === userdata.password){
       res.status(200).json({message: "login success"});
     }else{
       res.status(401).json({message: "Incorrect password"});
@@ -54,8 +40,8 @@ app.post('/signup', async (req, res) => {
   const users = db.collection('users');
   const userarr = await users.find({}).toArray();
   
-  founduser = userarr.find(x => x.username === userdata.username);
-  if(founduser){
+  const foundUser = userarr.find(x => x.username === userdata.username);
+  if(foundUser){
     res.status(409).json({message: "User already exists"})
   }else{
     users.insertOne(userdata);
@@ -63,21 +49,47 @@ app.post('/signup', async (req, res) => {
   }
 })
 
-app.get('/company', (req, res) => {
+app.get('/company:cname', async (req, res) => {
 
-  res.send("login info")
+  let companyName = req.params.cname.slice(1);
+  const jobs = db.collection('jobs');
+  const jobsarr = await jobs.find({company: companyName}).toArray();
+
+  if(jobsarr.length != 0){
+    res.status(200).json(jobsarr);
+  }else{
+    res.status(404).json("companynotfound");
+  }
+
 })
 
-app.get('/createjob', (req, res) => {
-    res.send("login info")
+app.post('/createjob', (req, res) => {
 })
 
-app.get('/jobs', (req, res) => {
-    res.send("login info")
+app.get('/jobs', async (req, res) => {
+    
+  const jobsarr = await db.collection('jobs').find({}).toArray();
+
+  let resarr = [];
+
+  for (let i=0; i<jobsarr.length; i++){
+    resarr.push({title: jobsarr[i].title, company: jobsarr[i].company, desc: jobsarr[i].description});
+  }
+
+  res.status(200).json(resarr);
 })
 
-app.get('/jobstatus:id', (req, res) => {
-    res.send("login info")
+app.get('/jobstatus:id', async (req, res) => {
+    
+  const jobID = req.params.id;
+  const jobsarr = await db.collection('jobs').find({}).toArray();
+
+  foundjob = jobsarr.find(x => x.ID = jobID);
+  if(foundjob){
+    res.status(200).json(foundjob.participants);
+  }else{
+    res.status(404).send("job not found");
+  }
 })
 
 app.listen(port, () => {

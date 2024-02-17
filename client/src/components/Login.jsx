9265fab10dc4de2b898b68c5868db632609d80a6
jatingaur18 <jatin22101@iiitnr.edu.in>
2024-02-17 10:24:55 +0530

    3rd

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,19 +1,51 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 const Login = () => {
+
+  const [User, setUser] = useState("");
+  const [Pass, setPass] = useState("");
+  const nav = useNavigate();
+  const submit = async () => {
+    const response = await fetch('http://localhost:3000/signup', {
+      method: "POST",
+      headers: {
+        'Content-Type': "application/json",
+      },
+      body: JSON.stringify({
+        email: Semail,
+        password: Spassword,
+      }),
+    })
+
+    if(response.status == 200){
+      if(response.body.type=="recruiter"){
+        nav('/Company')
+      }
+      else{
+        nav('/applicant')
+      }
+    }
+  }
+
+
+
+
   return(
     <div>
       <h1>Login</h1>
 
-      <form>
+      <form>    
       <label>
-        <p>Username</p>
-        <input type="text" />
+        <p>Username / Company name</p>
+        <input type="text" onChange={(e) => setUser(e.target.value)} />
       </label>
       <label>
         <p>Password</p>
-        <input type="password" />
+        <input type="password"  onChange={(e) => setPass(e.target.value)}/>
       </label>
       <div>
-        <button type="submit">Submit</button>
+      <button type="button" onClick={submit}>Login</button>
       </div>
     </form>
     </div>
